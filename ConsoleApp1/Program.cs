5697234b0cef151ab817ee3273d75a6022945aa2
makskayunPC <maks.kayn@yandex.com>
2021-11-25 11:19:22 +0300

    add UtilClass and Foo2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,12 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello!");
+            Foo2();
+        }
+
+        public static void Foo2()
+        {
+            Console.WriteLine(UtilClass.Sum(1,2));
         }
     }
 }
